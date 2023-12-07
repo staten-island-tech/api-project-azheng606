54fd1d12b9272a20f062e669fe17ffeb061d6c1f
azheng606 <144460945+azheng606@users.noreply.github.com>
2023-12-07 10:17:13 -0500

    s

---
+++ present/js/main.js
@@ -0,0 +1,35 @@
+import '../style/style.css'
+/* function greet (name) {
+const greetPromise =  new Promise(function (resolve, reject){
+    resolve(`Hello ${name}`);
+    //if you resvole it/find the data looking for, then it will return hello "the data"
+});
+return greetPromise
+}
+const mandy = greet ("Mandy");
+//handle the promise 
+mandy.then((result)=> {
+    //putting data into HTML
+console.log (result);
+});
+//get the data and then handle the data */
+
+ 
+const URL = "https://api.disneyapi.dev/character";
+
+async function getData(URL){
+    try {
+        const response = await fetch(URL);
+        if (response.status != 200){
+            throw new Error(response.statusText);
+        }
+        const all = await response.json();
+        document.querySelector(".gallery").textContent = all.name;
+        console.log (all.data.name);
+        console.log (all);
+    } catch (error) {
+        console.log(error, "UH OH");
+        document.querySelector(".gallery").textContent = "No thing founddddd"
+        
+    }
+} getData(URL)
