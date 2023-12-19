d3707ddfd02e0e1c67b4f3bea36ee55d68d2b675
azheng606 <144460945+azheng606@users.noreply.github.com>
2023-12-19 10:17:28 -0500

    w

--- present/js/dom.js
+++ present/js/dom.js
@@ -3,4 +3,7 @@ export {DOMSelectors}
 const DOMSelectors = {
     box:document.querySelector(".box"),
     gallery:document.querySelector(".gallery"),
+    start:document.querySelector(".start"),
+    first:document.querySelector(".first"),
+    last:document.querySelector(".last"),
 }
--- present/js/main.js
+++ present/js/main.js
@@ -26,6 +26,45 @@ async function getData() {
         const all = await response.json ();
         document.querySelector(".gallery").textContent = all.data;
         console.log (all)
+        const lolz = all.data.filter((data)=> data.films.length ===2 )
+        lolz.forEach(el=> {DOMSelectors.start.insertAdjacentHTML("beforeend",  `
+        <div class="card">
+        <div class ="name"> ${el.name}</div>
+        <img src= ${el.imageUrl} class="card-img">
+        </div>`)})     
+      } catch (error) {
+console.log(error, "UH OH");
+document.querySelector(".gallery").textContent = "No thing founddddd"   
+};
+} getData(URL) 
+
+
+DOMSelectors.form.addEventListener("submit" ,function (event) {
+    let hello = DOMSelectors.first.value 
+    let bye = DOMSelectors.last.value 
+    const pop = `https://api.disneyapi.dev/character?name=${hello}%20${bye}`
+ })
+
+
+/*
+<label for="site-search">Search the site:</label>
+<input type="search" id="site-search" name="q" />
+
+<button>Search</button>
+ */
+
+
+/* 
+
+async function getData() {
+    try {
+        const response = await fetch(URL);
+        if (response.status != 200){
+            throw new Error(response.statusText);
+        }
+        const all = await response.json ();
+        document.querySelector(".gallery").textContent = all.data;
+        console.log (all)
         const lolz = all.data.filter((data)=> data.films)
         lolz.forEach(el=> {DOMSelectors.box.insertAdjacentHTML("beforeend",  `
         <div class="card">
@@ -35,20 +74,4 @@ async function getData() {
       } catch (error) {
 console.log(error, "UH OH");
 document.querySelector(".gallery").textContent = "No thing founddddd"   
-};
-} getData(URL)
-
-
-
-
-
-
-const height = Number (prompt("enter your height in centimeters"));
-/* 
-
-
-<label for="site-search">Search the site:</label>
-<input type="search" id="site-search" name="q" />
-
-<button>Search</button>
- */
+}; */
